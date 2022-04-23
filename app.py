a6ba41cc8bfc7619c7ced422a74626b8ed2a9ec9
Garvit9000c <909garvit@gmail.com>
2022-04-24 00:47:10 +0530

    init

--- app.py
+++ app.py
@@ -17,6 +17,33 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def home():
     return render_template('home.html')
 
+@app.route('/', methods=['POST', 'GET'])
+def get_data():
+        if request.method == 'POST':
+                flag=request.form.get('lang')
+                if not(request.form.get('mode')):
+                        text = request.form['search']
+                        if flag:
+                                text=English(text)
+                        text=Simplifier(text)
+                else:
+                        file_=request.files['img']
+                        filename = secure_filename(file_.filename)
+                        s=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                        file_.save(s)
+                        text=Text_convertor(s,flag)
+                        text=Simplifier(text)
+                        if flag:
+                                text=English(text)
+                s2=os.path.join(app.config['UPLOAD_FOLDER'], 'name.txt')
+                with open(s2,'w') as f:
+                    f.write(text)     
+                if request.form.get('type'):
+                    return redirect(url_for('legal', name='Processing'))
+                else:
+                    return redirect(url_for('summary', name='Processing'))
+                
+
 
 if __name__ == '__main__' :
         app.run(debug=True)
